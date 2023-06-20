19af5c24d3310c59f6d3bd1c5c0cf523d6a98584
Nisha Saraswat <nishasaraswat@Nishas-MBP.lan>
2023-06-20 11:57:42 +0200

    first-commit

---
+++ components/sidebar/index.js
@@ -0,0 +1,90 @@
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import styles from "./sidebar.module.css";
+import classNames from "classnames";
+import { useUser, useSupabaseClient } from '@supabase/auth-helpers-react'
+import { useRouter } from "next/router";
+
+export default function Navbar() {
+  const supabaseClient = useSupabaseClient();
+  const router = useRouter();
+  const user = useUser();
+
+  let pathname = usePathname() || "/";
+  if (pathname.includes("/blog/")) {
+    pathname = "/blog";
+  }
+
+  const navItems = {
+    "/": {
+      name: "Home",
+    },
+    "/about": {
+      name: "About",
+    },
+    "/blog": {
+      name: "Blog",
+    },
+    "/create-post": {
+      name: "Create post",
+      requiresAuth: true,
+    },
+    "/login": {
+      name: "Login",
+      requiresAuth: false,
+    },
+    "/logout": {
+      name: "Logout",
+      requiresAuth: true,
+      onClick: async () => {
+        await supabaseClient.auth.signOut();
+        router.push('/login')
+        console.log("signout");
+      }
+    }
+  };  
+
+  return (
+    <aside className={styles.container}>
+      <div className={styles.sticky}>
+        <nav className={styles.navigation} id="nav">
+          <div className={styles.navigationItemWrapper}>
+            {Object.entries(navItems).map(([path, { name, requiresAuth, onClick }]) => {
+              const isActive = path === pathname;
+
+              if((requiresAuth && !user)|| (path === "/login" && user)){
+                return null;
+              }
+              if(path === "/logout"){
+                  return(
+                      <button
+                      className={classNames(styles.navigationItem, styles.navigationButton,{
+                        [styles.textNeutral]: !isActive,
+                        [styles.fontBold]: isActive,
+                      })}
+                       key={path}
+                       onClick={onClick}
+                       >
+                        {name}
+                      </button>
+                  )
+              }
+              return (
+                <Link
+                  key={path}
+                  href={path}
+                  className={classNames(styles.navigationItem, {
+                    [styles.textNeutral]: !isActive,
+                    [styles.fontBold]: isActive,
+                  })}
+                >
+                  <span className={styles.linkName}>{name}</span>
+                </Link>
+              );
+            })}
+          </div>
+        </nav>
+      </div>
+    </aside>
+  );
+}
---
+++ pages/login/index.jsx
@@ -0,0 +1,31 @@
+import { Auth } from '@supabase/auth-ui-react'
+import { ThemeSupa } from '@supabase/auth-ui-shared'
+import { useUser, useSupabaseClient } from '@supabase/auth-helpers-react'
+import { useEffect} from 'react'
+import { useRouter } from 'next/router'
+
+const LoginPage = () => {
+  const supabaseClient = useSupabaseClient()
+  const user = useUser()
+  const router = useRouter()
+  console.log(user);
+
+  useEffect(() => {
+    if(user){
+      router.push("/")
+    }
+
+  },[user, router])
+
+    return (
+      <Auth
+        redirectTo="http://localhost:3000/"
+        appearance={{ theme: ThemeSupa }}
+        supabaseClient={supabaseClient}
+        providers={[]}
+        socialLayout="horizontal"
+      />
+    )
+
+}
+export default LoginPage
